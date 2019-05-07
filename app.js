12ddf923132043a201f997dc6fd63fca348b2b63
Unknown <AlexOkonor@gmail.com>
2019-05-08 01:57:41 +0300

    Code formatting
    2 spaces

--- app.js
+++ app.js
@@ -47,16 +47,16 @@ class ColoredPyramidGeometry extends Geometry {
 
         // prettier-ignore
         positions: new Float32Array([
-           0,  1,  0,
+          0,  1,  0,
           -1, -1,  1,
-           1, -1,  1,
-           0,  1,  0,
-           1, -1,  1,
-           1, -1, -1,
-           0,  1,  0,
-           1, -1, -1,
+          1, -1,  1,
+          0,  1,  0,
+          1, -1,  1,
+          1, -1, -1,
+          0,  1,  0,
+          1, -1, -1,
           -1, -1, -1,
-           0,  1,  0,
+          0,  1,  0,
           -1, -1, -1,
           -1, -1,  1
         ]),
@@ -126,30 +126,29 @@ class ColoredCubeGeometry extends CubeGeometry {
 }
 
 class Camera{
-    constructor(pos, ang, wfront=new Vector3(0,0,1), wup=new Vector3(0,1,0)){
-        this.pos = pos;
-        this.ang = ang;
-        this.front = new Vector3();
-        this.right = new Vector3();
-        this.up = new Vector3();
-        this.wfront = wfront;
-        this.wup = wup;
-        this.viewMatrix = new Matrix4();
-        this.updateVectors();
-    }
-    updateVectors(){
-        this.viewMatrix = new Matrix4().rotateXYZ(this.ang).transpose();
-        this.front = new Vector3(this.viewMatrix.transformDirection(this.wfront));
-        this.up = new Vector3(this.viewMatrix.transformDirection(this.wup));
-        this.right = this.front.clone().cross(this.up).normalize();
-        this.viewMatrix.transpose().translate(this.pos);
-        console.log(this.pos, this.front, this.up, this.right)
-    }
-    updateCamera(dpos=new Vector3(0), dang=new Vector3(0)){
-        this.pos.add(dpos);
-        this.ang.add(dang);
-        this.updateVectors();
-    }
+  constructor(pos, ang, wfront=new Vector3(0,0,1), wup=new Vector3(0,1,0)){
+    this.pos = pos;
+    this.ang = ang;
+    this.front = new Vector3();
+    this.right = new Vector3();
+    this.up = new Vector3();
+    this.wfront = wfront;
+    this.wup = wup;
+    this.viewMatrix = new Matrix4();
+    this.updateVectors();
+  }
+  updateVectors(){
+    this.viewMatrix = new Matrix4().rotateXYZ(this.ang).transpose();
+    this.front = new Vector3(this.viewMatrix.transformDirection(this.wfront));
+    this.up = new Vector3(this.viewMatrix.transformDirection(this.wup));
+    this.right = this.front.clone().cross(this.up).normalize();
+    this.viewMatrix.transpose().translate(this.pos);
+  }
+  updateCamera(dpos=new Vector3(0), dang=new Vector3(0)){
+    this.pos.add(dpos);
+    this.ang.add(dang);
+    this.updateVectors();
+  }
 }
 
 
@@ -191,93 +190,94 @@ export default class AppAnimationLoop extends AnimationLoop {
     const view = this.camera.viewMatrix;
 
     pyramid
-      .setUniforms({
-        uPMatrix: projection,
-        uMVMatrix: view
-          .clone()
-          .translate([-1.5, 0, -8])
-          .rotateY(tick * 0.01)
-      })
-      .draw();
+    .setUniforms({
+      uPMatrix: projection,
+      uMVMatrix: view
+      .clone()
+      .translate([-1.5, 0, -8])
+      .rotateY(tick * 0.01)
+    })
+    .draw();
 
     const phi = tick * 0.01;
     cube
-      .setUniforms({
-        uPMatrix: projection,
-        uMVMatrix: view
-          .clone()
-          .translate([1.5, 0, -8])
-          .rotateXYZ([phi, phi, phi])
-      })
-      .draw();
+    .setUniforms({
+      uPMatrix: projection,
+      uMVMatrix: view
+      .clone()
+      .translate([1.5, 0, -8])
+      .rotateXYZ([phi, phi, phi])
+    })
+    .draw();
   }
 }
 const currentlyPressedKeys = {};
 function addKeyboardHandler(canvas) {
-    addEvents(canvas, {
-      onKeyDown(e) {
-        currentlyPressedKeys[e.code] = true;
-      },
-      onKeyUp(e) {
-        currentlyPressedKeys[e.code] = false;
-      }
-    });
+  addEvents(canvas, {
+    onKeyDown(e) {
+      currentlyPressedKeys[e.code] = true;
+    },
+    onKeyUp(e) {
+      currentlyPressedKeys[e.code] = false;
+    }
+  });
 }
 
 function addPointerHandler(canvas, camera) {
-    let mouseDown = false;
-    let currentX = 0;
-    let currentY = 0;
-    let moveCallback = function(e){
-        pointerPos(currentX+e.movementX, currentY+e.movementY);
-    }
+  let mouseDown = false;
+  let currentX = 0;
+  let currentY = 0;
+  let moveCallback = function(e){
+    camera.updateCamera(new Vector3(), new Vector3(e.movementY,e.movementX).scale(0.001));
+  }
 
-    document.addEventListener('pointerlockchange', function(e){
-        if (canvas === document.pointerLockElement) {
-            document.addEventListener("mousemove", moveCallback, false);
-        } else {
-            document.removeEventListener("mousemove", moveCallback, false);
-        }
-    }, false);
-    canvas.addEventListener('click', function(){
-        if(canvas !== document.pointerLockElement) canvas.requestPointerLock();
-        else document.exitPointerLock();
-      }, false);
-    addEvents(canvas, {
-      onDragMove: function(e){
-        if(canvas !== document.pointerLockElement) pointerPos(e.x, e.y);
-      },
-    });
-
-    let pointerPos = function(x, y){
-        const dx = x - currentX;
-        const dy = y - currentY;
-        camera.updateCamera(new Vector3(), new Vector3(dy,dx).scale(0.001));
-        currentX = x;
-        currentY = y;
+  document.addEventListener('pointerlockchange', function(e){
+    if (canvas === document.pointerLockElement) {
+      document.addEventListener("mousemove", moveCallback, false);
+    } else {
+      document.removeEventListener("mousemove", moveCallback, false);
     }
+  }, false);
+  canvas.addEventListener('click', function(e){
+    if(canvas !== document.pointerLockElement) canvas.requestPointerLock();
+    else document.exitPointerLock();
+  }, false);
+  addEvents(canvas, {
+    onDragStart: function(e){ currentX=e.x; currentY=e.y; },
+    onDragMove: function(e){
+      if(canvas !== document.pointerLockElement) pointerPos(e.x, e.y);
+    },
+  });
+
+  let pointerPos = function(x, y){
+    const dx = x - currentX;
+    const dy = y - currentY;
+    camera.updateCamera(new Vector3(), new Vector3(dy,dx).scale(0.001));
+    currentX = x;
+    currentY = y;
+  }
 }
 
 function updateCamera(camera){
-    let dpos = new Vector3(0);
-    if (currentlyPressedKeys[37] || currentlyPressedKeys[65]) {
-        // Left cursor key or A
-        dpos.subtract(camera.right);
-    } else if (currentlyPressedKeys[39] || currentlyPressedKeys[68]) {
-        // Right cursor key or D
-        dpos.add(camera.right);
-    }
-    if (currentlyPressedKeys[38] || currentlyPressedKeys[87]) {
-        // Up cursor key or W
-        dpos.add(camera.front);
-    } else if (currentlyPressedKeys[40] || currentlyPressedKeys[83]) {
-        // Down cursor key or S
-        dpos.subtract(camera.front);
-    }
+  let dpos = new Vector3(0);
+  if (currentlyPressedKeys[37] || currentlyPressedKeys[65]) {
+    // Left cursor key or A
+    dpos.subtract(camera.right);
+  } else if (currentlyPressedKeys[39] || currentlyPressedKeys[68]) {
+    // Right cursor key or D
+    dpos.add(camera.right);
+  }
+  if (currentlyPressedKeys[38] || currentlyPressedKeys[87]) {
+    // Up cursor key or W
+    dpos.add(camera.front);
+  } else if (currentlyPressedKeys[40] || currentlyPressedKeys[83]) {
+    // Down cursor key or S
+    dpos.subtract(camera.front);
+  }
 
-    dpos.scale(.1);
-    //if(dpos.dot(new Vector3())!==0)
-        camera.updateCamera(dpos, new Vector3(0));
+  dpos.scale(.1);
+  //if(dpos.dot(new Vector3())!==0)
+  camera.updateCamera(dpos, new Vector3(0));
 }
 
 
